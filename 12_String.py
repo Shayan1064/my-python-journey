2ec9f6d281ec48d725edc28a93040b5da71db931
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-09 06:55:52 +0500

    What is String

---
+++ 12_String.py
@@ -0,0 +1,12 @@
+str1=input("Enter str1 : ")
+str2=input("Enter str2 : ")
+
+#concatination (mean adding or combine two strings )
+
+final_str=str1+" "+str2 #" " is for spacing between two strings if you want
+
+print(final_str)
+#using len function
+#len function is to used to count 
+
+print(len(final_str))
