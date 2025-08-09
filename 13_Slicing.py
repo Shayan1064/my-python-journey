e28b535106b4a549489fb14eca7dfe0a30b59a60
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-09 06:56:45 +0500

    What is Slicing! Slicing Code

---
+++ 13_Slicing.py
@@ -0,0 +1,12 @@
+num1=input("Enter num1 : ")
+num2=input("Enter num2 : ")
+
+#Slicing 
+str3=num1+" "+num2
+print(str3)
+print()
+print(str3[3:6]) #Specific characters 
+print()
+print(str3[:6])  #From to start
+print()
+print(str3[0:])  #Upto End
