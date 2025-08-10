a3e00e05c6463217d415069209f42cdf4bab2446
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-10 14:32:51 +0500

    Tuple and tuple methods

---
+++ 19_Tuple.py
@@ -0,0 +1,10 @@
+movie=("Players","Rascals","Drishyam")
+print(movie)
+
+# Methods in Tuple
+print("The movie is in the index of : ")
+print(movie.index("Players"))
+
+print()
+
+print(movie.count("Rascals"))
