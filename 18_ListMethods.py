eecfdbf98bce8d576693b2f1555296185addfa5e
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-10 14:32:12 +0500

    List Functions & Methods

---
+++ 18_ListMethods.py
@@ -0,0 +1,31 @@
+#printing list
+Numbers=[2,1,3,5,6]
+print(Numbers)
+
+print()
+
+#Append method
+Numbers.append(4)
+print(Numbers)
+
+#sort method
+Numbers.sort()
+print(Numbers)
+
+#Sort but in decending
+Numbers.sort(reverse=True)
+print(Numbers)
+
+#Reverse the number
+Numbers.reverse()
+print(Numbers)
+
+#Remove the number
+Numbers.remove(len(Numbers))
+print(Numbers)
+
+#Insertion
+Numbers.insert(0,22)
+print(Numbers)
+
+ #and more...
