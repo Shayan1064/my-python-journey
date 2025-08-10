55c91f8361dc1da549ee23996f9c97273420b514
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-10 14:28:22 +0500

    String Methods & Functions

---
+++ 14_StringFunction.py
@@ -0,0 +1,6 @@
+str="Hello Shayan! Shayan is laerning Python"
+print(str.endswith("on")) #True if str is ending with on
+print(str.capitalize()) #it Capitalize the 1st char
+print(str.count("Shayan")) #Count that String or char
+print(str.find("Python")) #Find some character
+print(str.replace("o","a")) #Replacing with
