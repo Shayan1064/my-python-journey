e04da31d1b8846b585c43123cb1a6135e982624c
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-10 14:29:50 +0500

    Finding Greatest Number

---
+++ 16_GreatestNumber.py
@@ -0,0 +1,7 @@
+num1,num2,num3=28,32,244
+if(num1>num2 and num1>num3):
+    print(num1," IS GREATER")
+elif(num2>num1 and num2>num3):
+    print(num2," is greater")
+else:
+    print(num3, "is greater")
