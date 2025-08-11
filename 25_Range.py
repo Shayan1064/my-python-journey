e0740fba384247fdb935062f8f35b40778cf5c5d
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-11 22:13:53 +0500

    What is Range in Python

---
+++ 25_Range.py
@@ -0,0 +1,18 @@
+# 1st Method
+for i in range(5):
+    print(i)
+
+    #2nd Method
+for j in range(0,11):
+    print(j)
+
+#3rd Mrthod
+for k in range(0,100,10):
+        print(k)
+
+        #Multiplication
+
+n=int(input("Enter your Number :"))
+for s in range(1,17):
+     print(n*s)
+     
