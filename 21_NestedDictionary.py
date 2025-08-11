d6893cded97087ae3eb20c6eb68b878f516dea06
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-11 22:11:00 +0500

    Nested dictionary simple code

---
+++ 21_NestedDictionary.py
@@ -0,0 +1,10 @@
+Student={
+    "Name : " : "Shayan Hassan",
+    "Skills : " : {
+        "Skill 1 : " : "Facebook ad",
+        "Skill 2 : " : "YouTube ad",
+        "Skill 3 : " : "Instagram ad",
+        "Skill 4 : " : "Google ad",
+    }
+}
+print(Student)
