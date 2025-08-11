75ea336d546e5b9c1526371f7bfb95c2dd1a54fe
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-11 22:15:08 +0500

    For + While  + Range

---
+++ 26_PractiseProblemOnLOOPS.py
@@ -0,0 +1,23 @@
+#finding sum of all numbers
+
+sum=0
+for i in range(11):
+    sum+=i
+    print("The total is +",sum)
+
+#finding factorial of a number
+fac=1
+n=5
+i=1
+
+while i<=n:
+    fac*=i
+    i+=1
+
+print(fac)
+
+#Using For LOOP
+fac=1
+for i in range(1,6):
+    fac*=i
+print(fac)
