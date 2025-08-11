c085a05d243ae1557232d0e03a6d3e4305c20af7
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-11 22:10:20 +0500

    Dictionary in Python

---
+++ 20_Dictionary.py
@@ -0,0 +1,11 @@
+info={
+    "Name :" : "Shayan Hassan",
+    "Age : " :  21,
+    "Marks : " : 94.4,
+    "Subjects : " : ["Maths","Urdu"], #List
+    "Semester : " : ("ist","2nd") #Tuple
+
+}
+
+print(info)
+print(info["Name :"])
