35c816ec776ff5b0521f80d49b0be1ca1db67a43
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-12 11:21:52 +0500

    Function and Function basics

---
+++ 27_Function.py
@@ -0,0 +1,14 @@
+# def Avg(a,b,c):
+#     sum=a+b+c
+#     total=sum/3
+#     print(total)
+#     return Avg
+# Avg(2,3,4)
+  
+
+def square(a):
+    sq=a*a
+    print(sq)
+    return square
+square(4)
+square(16)
