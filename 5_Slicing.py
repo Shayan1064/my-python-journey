695d3277b84a1afb3bb3f43e0bb21cd6650831c5
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-13 07:26:40 +0500

    What is Slicing in Python

---
+++ 5_Slicing.py
@@ -0,0 +1,9 @@
+name="Shayan"
+
+# There are many method for slicing...
+
+print(name[0:]) # start from 0 and end eith last
+print(name[:5])  # start and upto 5
+print(name[0:len(name)])   # start and upto end
+
+print(name[1:4]) # mention both indexes
