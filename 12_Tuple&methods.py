51c2878d29271c6bd6a791f0a25924a9bb09443a
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-14 14:40:50 +0500

    Simple Tuple code and Methods

---
+++ 12_Tuple&methods.py
@@ -0,0 +1,17 @@
+student=("shayan",21,98.8,"Swabi")
+print(student)
+
+# Count
+print(student.count(21))
+
+# Index
+i=student.index("shayan")
+print(i)
+
+# Slicing
+sliceed=student[1:4]
+print(sliceed)
+# and more methods of slicing..
+
+
+# also more methods is used with tuple....
