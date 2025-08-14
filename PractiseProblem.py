8019a10d6b98a3e92a851ede3d1287ee6936798e
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-14 14:44:04 +0500

    Dictionary+Sets Practise Problem

---
+++ PractiseProblem.py
@@ -0,0 +1,34 @@
+# Question :01
+
+# write code to enter seven names and use the user input
+
+fruits=[]
+print("Enter your names")
+names=input()
+fruits.append(names)
+print(fruits)
+
+#---------------------------------------------------------------------------------------
+
+# Question :02
+
+# write code accept marks of five students and display it in sorted form
+
+marks=[]
+print("Enter your marks")
+mar=int(input())
+
+marks.append()
+sortedd=marks.sort()
+print(sortedd)
+
+
+#---------------------------------------------------------------------------------------
+
+# Question :03
+
+# write code to sum all number in List
+
+l=[2,44,5,6,44,7]
+print(sum(list))
+
