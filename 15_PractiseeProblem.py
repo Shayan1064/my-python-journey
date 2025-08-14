1960eedcf2ffb52c65a9673d6c40254341dacfe8
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-14 14:43:26 +0500

    Dictionary+Sets Pracrtise Problem

---
+++ 15_PractiseeProblem.py
@@ -0,0 +1,39 @@
+# # Question :1
+#             #  Write the code to to enter name of the word and its give you meaning
+
+# dictionary={
+#     "Help" : "Madad",
+#     "replace" : "Tabdeel",
+#     "Distance" : "Fasila",
+#     "Home" : "ghar",
+#     "Father" : "Baap"
+# }
+
+# word=input("Enter the word your to search the meaning :")
+# print(dictionary[word])
+
+#------------------------------------------------------------------------------------------------
+
+# Question :2
+            #  write code take input from user into dictionary and then print it
+
+dic={}
+name=input("Enter Name:")
+lang=input("Enter Language:")
+dic.update({name:lang})
+
+name=input("Enter Name:")
+lang=input("Enter Language:")
+dic.update({name:lang})
+
+name=input("Enter Name:")
+lang=input("Enter Language:")
+dic.update({name:lang})
+
+name=input("Enter Name:")
+lang=input("Enter Language:")
+dic.update({name:lang})
+
+print(dic)
+
+#------------------------------------------------------------------------------------------------
