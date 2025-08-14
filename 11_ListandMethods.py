3c528074dd502de1a63d49b0200efce59568cae2
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-14 14:39:36 +0500

    List and Methods

---
+++ 11_ListandMethods.py
@@ -0,0 +1,25 @@
+student=["Shayan",21.5,98,True]
+
+print(student)
+
+# apppend new word
+student.append("Swabi")
+print(student)
+
+# count the given word it may be any
+print(student.count("Shayan"))
+
+
+# sort the list in easy way
+number=[1,2,5,4,6,8,3,4,5,7,0]
+number.sort()
+print(number)
+
+
+# reverse the list 
+number.reverse()
+print(number)
+
+
+
+
