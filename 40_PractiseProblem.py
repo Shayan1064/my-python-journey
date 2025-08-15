2958cef9cd2212a4304fd7f3a1bb3e2d1742ce67
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-15 23:16:43 +0500

    Practise Problem

---
+++ 40_PractiseProblem.py
@@ -0,0 +1,10 @@
+
+
+# write code if shayan is prenst in post then print this post is about shayan
+
+psot=input("Paste a post")
+
+if("Shayan".lower() in psot.lower()):
+    print("This post is about Shayan")
+else:
+    print("This post is not about Shayan")
