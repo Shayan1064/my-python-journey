2aa905cce749e8629d03cfc858cac36643149c93
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-15 23:15:10 +0500

    If Else practise Problem

---
+++ 38_Length_Of_String.py
@@ -0,0 +1,12 @@
+
+
+# find the string is 10 charatcters or not
+
+print("Enter your word")
+word=input()
+
+if(len(word)>=10):
+    print("Yes it has 10 or more letters")
+else:
+    print("It has less than 10 characters")
+    
