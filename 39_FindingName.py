213d2473695286d386247045dcfbaef8ab1eb304
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-15 23:15:48 +0500

    How to find name in List

---
+++ 39_FindingName.py
@@ -0,0 +1,12 @@
+
+# Write code that tell weather your name is present in list or not
+
+names=["Shayan","Noman","Sanan","Hamdan","Hassan"]
+
+name=input("Enter name")
+
+if(name in names):
+    print("Yes you are presnet in List")
+else:
+    print("Your name is not present in List")
+    
