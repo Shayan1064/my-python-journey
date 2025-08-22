99cb0d7f3659392ca860feef20a8392703ac39b5
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-22 16:10:13 +0500

    Practise Problem 2

---
+++ 46_Practise_2.py
@@ -0,0 +1,21 @@
+
+
+# write a fucntion which convert celcius into fahrenhiet
+
+def c_to_f():
+    celcius=int(input("Enter C"))
+    fah=(celcius*9/5)+32
+    print(fah)
+
+c_to_f()
+
+
+# write a fucntion which convert fahrenhiet into celcius
+
+def f_to_c():
+
+    fahrentiheit=float(input("Enter : "))
+    cal=(fahrentiheit-32)*5/9
+    print(cal)
+
+f_to_c()
